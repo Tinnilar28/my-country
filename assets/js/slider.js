be7a7a341edec76ac27a86437c8fdbfa408b787c
Tinnilar28 <tinnilarlwin1996@gmail.com>
2025-05-21 17:37:35 +0900

    add slider pages

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,9 +1,17 @@
 const slides = document.querySelectorAll('.slide');
+// slide{
+//   <img src="https://placehold.jp/3d4070/ffffff/1024x512.png" class="slide show" alt="スライド1">
+//   <img src="https://placehold.jp/3e7051/ffffff/1024x512.png" class="slide" alt="スライド2">
+//   <img src="https://placehold.jp/ce0d0d/ffffff/1024x512.png" class="slide" alt="スライド3"></img>
+// }
+
 const prevBtn = document.querySelector('.prev-button');
+// prvButt = <button> class="prev-button">&#10094;</button> 
+
 const nextBtn = document.querySelector('.next-button');
 
 let current = 0;
-let timer = setInterval(showNextSlide, 3000); // 3秒で自動切り替え
+let timer = setInterval(showNextSlide, 3000); // 3000 = 3秒で自動切り替え 
 
 function updateSlide(index) {
   slides.forEach(slide => slide.classList.remove('show'));
